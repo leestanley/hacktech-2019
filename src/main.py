4056eb95a9b56c2ce8dffb077fa64753cc37c753
Kartik Nighojkar <karsohn@gmail.com>
2019-03-02 10:26:23 -0800

    Changed directory structure for project

--- src/main.py
+++ src/main.py
@@ -1,2 +1,13 @@
 import numpy as np
 import pandas as pd
+from flask import Flask
+from flask import url_for, jsonify, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+if __name__ == "__main__":
+    app.run(port=8080, debug=True)
